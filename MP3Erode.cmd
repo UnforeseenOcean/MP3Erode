01f3bccb6a26786cdff1fb7e0efd7c20b7194b15
Sean Canned Bean <UnforeseenOcean@users.noreply.github.com>
2016-08-07 15:16:34 +0900

    Updated - Added Functionality
    Now with MP3-WAV-MP3 loss!

--- MP3Erode.cmd
+++ MP3Erode.cmd
@@ -1,8 +1,32 @@
-set /a inc1=0
-set /a inc2=1
+@echo off
+echo Advanced MP3 Degrader
+echo 2016 Blackwood, No Rights Reserved
+echo Thanks for checking my stuff out!
+echo.
+echo Please name a WAV file as ufile(starting position).wav
+echo And put it in the same directory as the script!
+echo.
+set /p countin=Set starting position (Type 0 to start over): 
+set /p stopcount=Stop after nth file: (Type desired range, minimum 1): 
+
+set /a countout=%countin%
+set /a countwavo=%countin%
+set /a delcache=%countin%
+set /a countwavi=%countin%+1
+
 :start
-ffmpeg -i %inc1%.mp3 -codec:a libmp3lame -qscale:a 5 %inc2%.mp3
-set /a inc1=inc1+1
-set /a inc2=inc2+1
-::timeout /t 3 >nul
-goto start
+ffmpeg -i ufile%countwavo%.wav -codec:a libmp3lame -qscale:a 1 file%countout%.mp3
+ffmpeg -i file%countout%.mp3 -acodec pcm_u8 -ar 44100 ufile%countwavi%.wav
+set /a countin=%countin%+1
+set /a countout=%countout%+1
+set /a countwavi=%countwavi%+1
+set /a countwavo=%countwavo%+1
+:: Set the serial number of cache
+set /a delcache=%countout%-1
+:: Delete cache file
+del ufile%delcache%.wav
+if exist file%stopcount%.mp3 (goto stop) else goto start
+:stop
+echo.
+echo Queue finished
+pause
